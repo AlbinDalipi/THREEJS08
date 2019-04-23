f12091afe60aea8f9859ba2d5d156153abac5dab
AlbinDalipi <Albin5Dalipi@gmail.com>
2019-04-23 20:40:30 +0200

    Add files via upload

---
+++ js/dataJSON.js
@@ -0,0 +1,45 @@
+var dataJSON =
+    {
+        panoramas:
+            [{name:"vipsaloti", highres: 'https://i.imgur.com/ds4CeaB.jpg', lowres: 'https://i.imgur.com/ds4CeaBl.jpg'},
+                {name:"shkallet 0",highres: '//i.imgur.com/TJj125P.jpg', lowres: '//i.imgur.com/TJj125Ph.jpg'},
+                {name:"korridor 0",highres: '//i.imgur.com/aDpgILS.jpg', lowres: '//i.imgur.com/aDpgILSh.jpg'},
+                {name:"korridor 1",highres: '//i.imgur.com/DngfkDA.jpg', lowres: '//i.imgur.com/DngfkDAh.jpg'},
+
+            ],
+
+
+
+                vipsaloti:[
+                {
+                 name : 'hyrja',
+                 posx : 0.4,
+                 posy : 0.5,
+                 posz : 0.4,
+                 scalex : 0.97,
+                 scaley  : 0.97,
+                 scalez  : 0.97,
+
+
+                },
+
+                    {
+                        name : 'korridori',
+                        posx : 0.2,
+                        posy : 0.4,
+                        posz : 0.2,
+                        scalex : 0.97,
+                        scaley  : 0.97,
+                        scalez  : 0.97,
+
+
+                    }
+
+
+                ,
+
+            ]
+
+
+};
+
